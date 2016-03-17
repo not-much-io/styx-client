8e5a5efacd9be0c2aec371901eb47229c8a2ffe7
not-much-io <kristo.koert@gmail.com>
2016-03-17 16:32:33 +0200

    UI to dark theme and added shadows.

--- src/cljs/styx_client/views.cljs
+++ src/cljs/styx_client/views.cljs
@@ -3,10 +3,10 @@
             [goog.dom :as gdom]))
 
 (defn top-bar []
-  [:div#top-bar.flex.main-color.border-bottom
-   [:img.circle.m1.profile-picture {:src    "https://avatars.slack-edge.com/2015-05-30/5147732825_d04b8e601de9c90bed00_192.jpg"
-                    :height 55
-                    :width  55}]
+  [:div#top-bar.flex.main-color.border-bottom.card-4
+   [:img.circle.m1.profile-picture.card-2 {:src    "https://avatars.slack-edge.com/2015-05-30/5147732825_d04b8e601de9c90bed00_192.jpg"
+                                    :height 55
+                                    :width  55}]
    [:div
     [:h3#contact-name.mb0 "Kristo Koert"]
     [:h5#status.m0 "online"]]])
@@ -24,32 +24,44 @@
                              top-bar-height
                              bottom-bar-height)
                           "px")))
-             16)]                                           ; Performance hit?
+             16)
+        from-msg (fn [i m]
+                   [:div.flex.flex-wrap.ml2
+                    ^{:key i}
+                    [:div.col-12
+                     [:div.message1.secondary-color1.main-text.p2.rounded.card-3 m]]
+                    [:div.col-12
+                     [:h6.secondary-text.my1 "Sent 9.15"]]])
+        user-msg   (fn [i m]
+                     [:div.flex.flex-wrap.mr2
+                      ^{:key i}
+                      [:div.col-12
+                       [:div.message2.secondary-color2.main-text.p2.rounded.ml-auto.card-3 m]]
+                      [:div.col-12
+                       [:h6.secondary-text.my1.right-align "Sent 9.15"]]])]
     [:div#chat-feed
      (map-indexed
        (fn [idx msg]
          (if (even? idx)
-           ^{:key idx}
-           [:div.message1.p2.rounded.mr-auto.m3
-            msg]
-           ^{:key idx}
-           [:div.message2.p2.rounded.ml-auto.m3
-            msg]))
+           (from-msg idx msg)
+           (user-msg idx msg)))
        messages)]))
 
 (defn message-box []
   [:div#bottom-bar.border-top.main-color
    [:div.flex
-    [:div.col-2
-     [:i.material-icons.m2 "attachment"]]
-    [:div.col-6
-     [:textarea#chat-box.main-color.m2
+    [:div.col-1
+     [:i.material-icons.m2.c-s2-text "attachment"]]
+    [:div.col-7
+     [:textarea#chat-box.secondary-text.main-color.m2
       {:rows 1}
       "Type your message..."]]
-    [:div.col-2
-     [:i.material-icons.m2 "insert_emoticon"]]
-    [:div.col-2
-     [:i.material-icons.m2 "send"]]]])
+    [:div.col-1]
+    [:div.col-1
+     [:i.material-icons.m2.c-s2-text "insert_emoticon"]]
+    [:div.col-1
+     [:i.material-icons.m2.m2.c-s2-text "send"]]
+    [:div.col-1]]])
 
 (defn main-panel []
   (let [messages (re-frame/subscribe [:messages])]
