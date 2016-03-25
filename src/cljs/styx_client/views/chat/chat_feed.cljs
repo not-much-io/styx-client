88626e64778d4bba108080c735b4987f12cd0abb
not-much-io <kristo.koert@gmail.com>
2016-03-25 10:29:38 +0200

    Scrolling button done.

--- src/cljs/styx_client/views/chat/chat_feed.cljs
+++ src/cljs/styx_client/views/chat/chat_feed.cljs
@@ -9,27 +9,25 @@
 ;; REVIEW: Is this a good way to do this?
 (def id_chat_feed "chat-feed")
 
-(defn msg-in [k m]
+(defn- msg-in [k m]
   ^{:key k}
   ;; HACK: Simplify markup
-  [:div.msg-in
-   [:div.ml2.mt1
-    [:div.flex.flex-wrap
-     [:div.col-12
-      [:div.message-in.bg-color2.text-main.p2.rounded.shadow3 m]]
-     [:div.col-12
-      [:h6.text-secondary.my1 "Sent 9.15"]]]]])
+  [:div.ml2.mt1
+   [:div.flex.flex-wrap
+    [:div.col-12
+     [:div.message-in.bg-color2.text-main.p2.rounded.shadow3 m]]
+    [:div.col-12
+     [:h6.text-secondary.my1 "Sent 9.15"]]]])
 
-(defn msg-out [k m]
+(defn- msg-out [k m]
   ^{:key k}
   ;; HACK: Simplify markup
-  [:div.msg-out
-   [:div.mr2.mt1
-    [:div.flex.flex-wrap
-     [:div.col-12
-      [:div.message-out.bg-color3.text-main.p2.rounded.ml-auto.shadow3 m]]
-     [:div.col-12
-      [:h6.text-secondary.my1.right-align "Sent 9.15"]]]]])
+  [:div.mr2.mt1
+   [:div.flex.flex-wrap
+    [:div.col-12
+     [:div.message-out.bg-color3.text-main.p2.rounded.ml-auto.shadow3 m]]
+    [:div.col-12
+     [:h6.text-secondary.my1.right-align "Sent 9.15"]]]])
 
 (defonce _
          ;; Make chat feed take remaining height between top bar and bottom bar.
@@ -48,12 +46,12 @@
                            (str x "px")))))                 ; available height between top and bottom bar
            16))
 
-(defn scroll! [el start end time]
+(defn- scroll! [el start end time]
   "Plays a animation that will scroll an element from A to B.
   Start and End should be 2 dimensional arrays. [left, top]"
   (.play (gfx-dom/Scroll. el (clj->js start) (clj->js end) time)))
 
-(defn scrolled-to-bottom? [id]
+(defn- scrolled-to-bottom? [id]
   ;; TODO: add tolerance
   (let [el (gdom/getElement id)]
     (= (.-scrollTop el)
@@ -70,31 +68,42 @@
                    [0 (.-scrollHeight feed)]
                    500))))
 
-(defn auto-scroll-button [scrolled-to-bottom]
-    (if-not @scrolled-to-bottom
-      [:div.border "Scroll!"]
-      [:div]))
+(defn- auto-scroll-button
+  [should-scroll]
+  ;; FIXME: Fix edge cases with scrolling
+  ;;        1) Scrolling up when new message appears
+  ;;        2) Auto scroll button not showing after wp dimension change
+  (if-not (and @should-scroll
+               (scrolled-to-bottom? id_chat_feed))
+    [:div.absolute
+     {:style {:width "100%"
+              :bottom "5em"}}
+     [:div.flex
+      [:div.col-5]
+      [:div.col-2.center
+       {:style    {:background-color "black"
+                   :opacity          ".5"}
+        :on-click (macros/handler-fn
+                    (reset! should-scroll true))}
+       [:i.material-icons.text-color2.m2 "expand_more"]]
+      [:div.col-5]]]
+    [:div]))
 
 (defn chat-feed []
   (let [messages           (re-frame/subscribe [:sub-to [:messages]])
-        should-scroll      (reagent/atom false)
-        scrolled-to-bottom (reagent/atom false)]
+        should-scroll      (reagent/atom false)]
     (fn []
       (handle-scroll should-scroll)
       [:div#chat-feed
-       {:on-click  (macros/handler-fn
-                     (swap! should-scroll not))
-        :on-scroll (macros/handler-fn
+       {:on-scroll (macros/handler-fn
                      (if (scrolled-to-bottom? id_chat_feed)
-                       (do (reset! should-scroll true)
-                           (reset! scrolled-to-bottom true))
-                       (do (reset! should-scroll false)
-                           (reset! scrolled-to-bottom false))))}
+                       (reset! should-scroll true)
+                       (reset! should-scroll false)))}
        [anim/css-trans-group {:transition-name "feed"
                               :transition-enter-timeout 5000
                               :transition-leave-timeout 5000}
-        (for [{:keys [in-or-out key msg]} (reverse @messages)]
-          (if (= :out in-or-out)
+        (for [{:keys [in-or-out? key msg]} (reverse @messages)]
+          (if (= :out in-or-out?)
             (msg-out key msg)
-            (msg-in  key msg)))]
-       [auto-scroll-button scrolled-to-bottom]])))
+            (msg-in key msg)))]
+       [auto-scroll-button should-scroll]])))
